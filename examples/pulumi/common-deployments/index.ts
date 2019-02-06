c7b5c2b7aed57960f1bc9b33535176382ee9e168
joeduffy <joe@pulumi.com>
2019-02-05 19:23:01 -0800

    Implement the basic Velero resource provisioning
    This commit adds the Velero resources that are agnostic to any
    cloud provider. This includes the Velero CRDs and the Deployment
    and restic DaemonSet. That said, it's not currently possible to
    run the program yet, since doing so requires at least one cloud
    implementation to be available.

---
+++ examples/pulumi/common-deployments/index.ts
@@ -0,0 +1,133 @@
+// Copyright 2018 the Heptio Ark contributors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import * as k8s from "@pulumi/kubernetes";
+import * as k8stypes from "@pulumi/kubernetes/types/input";
+import { labels, namespace, serviceAccountName } from "../common-prereqs";
+
+// VeleroCloudSettings defines the cloud-specific aspects of a Velero deployment. Most of
+// the configuration between clouds can be shared, however certain aspects -- like selectors,
+// volumes, and particularly credentials -- differ. This interface abstracts those away.
+export interface VeleroCloudSettings {
+    env?: k8stypes.core.v1.EnvVar[];
+    envFrom?: k8stypes.core.v1.EnvFromSource[];
+    nodeSelector?: {[key: string]: string};
+    volumes?: k8stypes.core.v1.Volume[];
+    volumeMounts?: k8stypes.core.v1.VolumeMount[];
+}
+
+// deployVeleroObjects instantiates the associated Kubernetes objects, specific to the cloud
+// settings provided, into the target cluster.
+export function deployVeleroObjects(settings: VeleroCloudSettings) {
+    // Deploy the Velero container image into the cluster with the appropriate volumes wired up.
+    const veleroDeployment = new k8s.apps.v1beta1.Deployment("velero", {
+        metadata: { namespace },
+        spec: {
+            replicas: 1,
+            template: {
+                metadata: {
+                    labels,
+                    annotations: {
+                        "prometheus.io/scrape": "true",
+                        "prometheus.io/port": "8085",
+                        "prometheus.io/path": "/metrics",
+                    },
+                },
+                spec: {
+                    restartPolicy: "Always",
+                    serviceAccountName,
+                    volumes: (settings.volumes ? settings.volumes : []).concat([
+                        { name: "plugins", emptyDir: {} },
+                        { name: "scratch", emptyDir: {} },
+                    ]),
+                    containers: [{
+                        name: "velero",
+                        image: "gcr.io/heptio-images/velero:latest",
+                        ports: [{ name: "metrics", containerPort: 8085 }],
+                        command: [ "/velero" ],
+                        args: [
+                            "server",
+                            // uncomment following line and specify values if needed for multiple provider snapshot locations
+                            // "--default-volume-snapshot-locations=<provider-1:location-1,provider-2:location-2,...>",
+                        ],
+                        volumeMounts: (settings.volumeMounts ? settings.volumeMounts : []).concat([
+                            { name: "plugins", mountPath: "/plugins" },
+                            { name: "scratch", mountPath: "/scratch" },
+                        ]),
+                        env: (settings.env ? settings.env : []).concat([
+                            { name: "VELERO_SCRATCH_DIR", value: "/scratch" },
+                        ]),
+                        envFrom: settings.envFrom,
+                    }],
+                    nodeSelector: settings.nodeSelector,
+                },
+            },
+        },
+    });
+
+    // Finally deploy the restic DaemonSet.
+    const resticDaemonSet = new k8s.apps.v1.DaemonSet("restic", {
+        metadata: { namespace },
+        spec: {
+            selector: {
+                matchLabels: { name: "restic" },
+            },
+            template: {
+                metadata: {
+                    labels: { name: "restic" },
+                },
+                spec: {
+                    serviceAccountName,
+                    securityContext: { runAsUser: 0 },
+                    volumes: (settings.volumes ? settings.volumes : []).concat([
+                        {
+                            name: "host-pods",
+                            hostPath: { path: "/var/lib/kubelet/pods" },
+                        },
+                        { name: "scratch", emptyDir: {} },
+                    ]),
+                    containers: [{
+                        name: "velero",
+                        image: "gcr.io/heptio-images/velero:latest",
+                        command: [ "/velero" ],
+                        args: [ "restic", "server" ],
+                        volumeMounts: (settings.volumeMounts ? settings.volumeMounts : []).concat([
+                            {
+                                name: "host-pods",
+                                mountPath: "/host_pods",
+                                mountPropagation: "HostToContainer",
+                            },
+                            { name: "scratch", mountPath: "/scratch" },
+                        ]),
+                        env: (settings.env ? settings.env : []).concat([
+                            {
+                                name: "NODE_NAME",
+                                valueFrom: { fieldRef: { fieldPath: "spec.nodeName" } },
+                            },
+                            {
+                                name: "VELERO_NAMESPACE",
+                                valueFrom: { fieldRef: { fieldPath: "metadata.namespace" } },
+                            },
+                            {
+                                name: "VELERO_SCRATCH_DIR",
+                                value: "/scratch",
+                            },
+                        ]),
+                        envFrom: settings.envFrom,
+                    }],
+                },
+            },
+        },
+    });
+}
---
+++ examples/pulumi/common-prereqs/index.ts
@@ -0,0 +1,76 @@
+// Copyright 2017 the Heptio Ark contributors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import * as k8s from "@pulumi/kubernetes";
+
+// labels is a common label shared amongst many resources below.
+export const labels = { component: "velero" };
+
+// Provision all the different kinds of CRDs that Velero supports.
+const crds = [
+    { name: "backups", kind: "Backup" },
+    { name: "schedules", kind: "Schedule" },
+    { name: "restores", kind: "Restore" },
+    { name: "downloadrequests", kind: "DownloadRequest" },
+    { name: "deletebackuprequests", kind: "DeleteBackupRequest" },
+    { name: "podvolumebackups", kind: "PodVolumeBackup" },
+    { name: "podvolumerestores", kind: "PodVolumeRestores" },
+    { name: "resticrepositories", kind: "ResticRepository" },
+    { name: "backupstoragelocations", kind: "BackupStorageLocation" },
+    { name: "volumesnapshotlocations", kind: "VolumeSnapshotLocation" },
+    { name: "serverstatusrequests", kind: "ServerStatusRequest" },
+    { name: "serverstatusrequests", kind: "ServerStatusRequest" },
+];
+for (const crd of crds) {
+    new k8s.apiextensions.v1beta1.CustomResourceDefinition(`${crd.name}.velero.io`, {
+        metadata: { labels },
+        spec: {
+            group: "velero.io",
+            version: "v1",
+            scope: "Namespaced",
+            names: {
+                plural: crd.name,
+                kind: crd.kind,
+            },
+        },
+    });
+}
+
+// Create a namespace that all subsequent objects will be provisioned within.
+export const namespace =
+    new k8s.core.v1.Namespace("velero").metadata.apply(m => m.name);
+
+// Create the `velero` ServiceAccount and RBAC rules to bind it to our cluster.
+const serviceAccount = new k8s.core.v1.ServiceAccount("velero", {
+    metadata: {
+        namespace,
+        labels,
+    }
+});
+export const serviceAccountName = serviceAccount.metadata.apply(m => m.name);
+new k8s.rbac.v1beta1.ClusterRoleBinding("velero", {
+    metadata: {
+        labels,
+    },
+    subjects: [{
+        kind: "ServiceAccount",
+        namespace,
+        name: serviceAccountName,
+    }],
+    roleRef: {
+        kind: "ClusterRole",
+        name: "cluster-admin",
+        apiGroup: "rbac.authorization.k8s.io",
+    }
+});
